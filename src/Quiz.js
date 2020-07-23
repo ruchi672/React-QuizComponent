0aae285549bc1725f04e824735855ceca79ee2a7
ruchi672 <61773688+ruchi672@users.noreply.github.com>
2020-07-23 13:47:36 +0530

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,3 +1,26 @@
 import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
+
+class Quiz extends Component{
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Quiz
