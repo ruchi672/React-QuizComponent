e3fc07e63722cfde01936e4cc000f2d0c9624633
ruchi672 <61773688+ruchi672@users.noreply.github.com>
2020-07-23 13:53:26 +0530

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,6 +3,14 @@ import React, { Component } from 'react'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component{
+  constructor(props){
+   super(props) 
+  }
+  render(){
+    return (
+      <div>Quiz</div>
+      )
+  }
   
 }
 
